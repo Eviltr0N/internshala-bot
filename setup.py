bb1544729deca80f1ca52f89775d562df0995100
eviltr0n <mayanksing970@gmail.com>
2024-09-16 01:57:34 +0530

    ver bumped

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name="internshala-bot",
-    version="0.0.6",
+    version="0.0.7",
     author="Mayank Lodhi",
     author_email="",
     description="Package to automate internship application process on Internshala using ChatGPT.",
