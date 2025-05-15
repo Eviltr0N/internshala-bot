dd97b9b29526ef030338b6349faea2bc685bd7f4
eviltr0n <mayanksing970@gmail.com>
2025-05-15 23:22:03 +0530

    added chatgpt api(pollinations)

--- internshala_bot/chat_gpt.py
+++ internshala_bot/chat_gpt.py
@@ -8,22 +8,68 @@ from undetected_playwright.sync_api import sync_playwright, TimeoutError
 import json
 import os
 import time
-from .resume_handler import Skills, Certifications, Projects, cover_letter_prompt_format, assignment_prompt_format, assignment_validation_prompt
+import requests
+from internshala_bot.resume_handler import Skills, Certifications, Projects, cover_letter_prompt_format, assignment_prompt_format, assignment_validation_prompt
 
+# check is gpt api is alive at beggining then set the flag accordingly
+
+# setup a cli option to check if someone wants to use api or not  
+
+def is_gpt_api_alive() -> bool:
+    try:
+        res = requests.get("https://text.pollinations.ai/prompt=Hello", timeout = 5)
+        if res.status_code != 200:
+            return False
+        return True
+    except requests.exceptions.Timeout as e:
+        return False
 
 
 class chat:
-    def __init__(self, browser_inst):
+    def __init__(self, browser_inst, use_api = True):
         self.main_page_url = "https://chatgpt.com/?model=auto"
         self.cover_letter_url = None
         self.assignment_url = None
         self.gpt_check_asg_url = None
-        config_dir = os.path.join(os.getcwd(), '.config')
-        self.gpt_state_conf = os.path.join(config_dir, 'chat_gpt_state.json')
-        self.page = browser_inst.new_page()
+        self.use_api = use_api
+        
+        if not self.use_api:
+            config_dir = os.path.join(os.getcwd(), '.config')
+            self.gpt_state_conf = os.path.join(config_dir, 'chat_gpt_state.json')
+            self.page = browser_inst.new_page()
+        
+    def get_gpt_api_resp(self, prompt, json_mode = False):
+        print("send to gpt")
+        pmt = {
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ],
+            "model": "openai",
+            "jsonMode": json_mode
+            }
+        try:
+            resp = requests.post("https://text.pollinations.ai/", json=pmt, timeout = 20)
+            return resp.text
+        except requests.exceptions.Timeout as e:
+            print("TIMEOUTTTTTTT")
+
 
     def get_cover_letter(self, profile, company, about, skills, is_int_or_job):
         prompt = cover_letter_prompt_format(profile, company, about, skills, is_int_or_job)
+        if self.use_api:
+            resp = self.get_gpt_api_resp(prompt)
+            if len(resp) > 2048:
+                cover_lt=''
+                for x in resp.split("."):
+                    if len(cover_lt) + len(x) + 1 <= 2000:
+                        cover_lt += x + "."
+                    else:
+                        break
+            else:
+                cover_lt = resp
+            return cover_lt
+
         if self.cover_letter_url is None:
             try:
                 self.page.goto(self.main_page_url , timeout=30000, wait_until='networkidle')
@@ -71,6 +117,8 @@ class chat:
 
     def get_assignment_answer(self, profile, company, about, skills, question, is_int_or_job):
         prompt = assignment_prompt_format(profile, company, about, skills, question, is_int_or_job)
+        if self.use_api:
+            return self.get_gpt_api_resp(prompt)
 
         if self.assignment_url is None:
             try:
@@ -109,6 +157,10 @@ class chat:
 
     def assmnt_is_valid(self, profile, question, is_int_or_job):
         prompt = assignment_validation_prompt(profile, question, is_int_or_job)
+        if self.use_api:
+            resp = self.get_gpt_api_resp(prompt, json_mode=True)
+            return json.loads(resp)
+
         if self.gpt_check_asg_url is None:
             try:
                 self.page.goto(self.main_page_url , timeout=30000, wait_until='networkidle')
